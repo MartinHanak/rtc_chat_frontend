9b1b8acc8ad7d25378b9ced18945b3883fd35cea
Martin Hanák <martinhanak97@gmail.com>
2023-09-07 10:23:30 +0200

    Local video stream

--- src/app/room/[room]/components/RoomContent.tsx
+++ src/app/room/[room]/components/RoomContent.tsx
@@ -1,9 +1,14 @@
 import { userInfo } from "@/app/types/socketTypes";
 import { useSocketContext } from "./SocketContext";
+import { useLocalStreamContext } from "./LocalStreamContext";
+import { Video } from "./Video";
 
 
 export function RoomContent() {
+
     const { users } = useSocketContext();
+
+    const { streamRef } = useLocalStreamContext();
 
     return (
         <div>
@@ -12,6 +17,9 @@ export function RoomContent() {
             <ul>
                 {users.map((user: userInfo) => <li key={user.socketId}>{user.username}</li>)}
             </ul>
+
+            {/* Local Stream */}
+            {streamRef && streamRef.current && <Video stream={streamRef.current} />}
         </div>
     );
 }
